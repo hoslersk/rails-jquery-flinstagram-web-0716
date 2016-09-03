b2a4a79fb23f40a38d888caf8f7dc5a88999d47d
Schyler Hosler <schylerhosler@gmail.com>
2016-09-03 15:40:38 -0400

    Initial commit:

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,11 @@
 // Add your JavaScript here
+$(function(){
+  addLikeEventHandler();
+});
+
+function addLikeEventHandler(){
+  $('.like_button').on('ajax:complete', function(e, request, status){
+     $(this).find('span').toggleClass('red-heart grey-heart')
+     this.nextElementSibling.innerHTML = request.responseJSON.message;
+  });
+}
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,9 +5,20 @@ class PicturesController < ApplicationController
   end
 
   def like
+    # binding.pry
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-    redirect_to root_path
+    message = @picture.likes_message(current_user)
+    respond_to do |f|
+      # binding.pry
+      f.json { render json: { message: message }}
+      # if @picture.update_likes(current_user)
+      #   f.json { render json: {message: message} }
+      #   # f.html { redirect_to root_path }
+      # else
+      #   render :'/application/index'
+      # end
+    end
   end
-
+  # redirect_to root_path
 end
